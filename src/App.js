e21ad3536a24d7e0b1b91850c940bf8ec133a123
SlickChris95 <torres.christian33@yahoo.com>
2019-09-02 23:27:13 -0700

    created List component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import List from './List'
 
 class App extends React.Component {
+  /* term is for values passed, items is to store values
+  passed to todo List*/
+  state = {term: '', items: []};
+
+  onChange = (e)=> {
+    this.setState({term: e.target.value})
+  }
+  onSubmit = (e)=> {
+    e.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items,this.state.term]
+    });
+  }
+
   render(){
     return(
-      <div>Hello World</div>
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
     )
   }
 }
